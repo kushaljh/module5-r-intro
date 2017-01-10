af2862d243fb84fa9c846d23cc3151ff49b815b3
Kushal Jhunjhunwalla <kushal.jhunjhunwalla@gmail.com>
2017-01-10 15:04:59 -0800

    add solution to exercise-2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -22,4 +22,4 @@ total.cost <- puppies * puppy.price
 too.expensive <- total.cost > 1000
 
 # Create a variable `max.puppies`, which is the number of puppies you can afford for $1,000.
-max.puppies <- floor(1000/puppy.price)
+max.puppies <- floor(1000 / puppy.price)
